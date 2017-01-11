7004907075e8b0debc899dff4d08b5e48773d076
Hui Jun <hjyam@umich.edu>
2017-01-11 09:07:19 -0800

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :update]
     resource :session, only: [:create, :destroy]
+    resources :cities, only: [:index, :destroy]
+    resources :cities, only: [:show] do
+      resources :memberships, only: [:create, :destroy]
+    end
   end
 end
