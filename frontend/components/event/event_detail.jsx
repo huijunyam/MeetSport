d55243007781e447963a0bcdc74a15b338cdc107
Hui Jun <hjyam@umich.edu>
2017-01-16 02:13:20 -0800

    Complete styling event detail page

--- frontend/components/event/event_detail.jsx
+++ frontend/components/event/event_detail.jsx
@@ -8,30 +8,30 @@ class EventDetail extends React.Component {
       <div className="event-home-list">
         <div className="event-page col-last">
           <ul className="event-detail">
-            <li className="event-detail-item-date"><strong>Date: </strong>{eventDetail.date}</li>
-            <li className="event-detail-item-time"><strong>Start Time: </strong>{eventDetail.start_time}</li>
-            <li className="event-detail-item-time"><strong>End Time: </strong>{eventDetail.end_time}</li>
-            <li className="event-detail-item"><strong>Location: </strong>{eventDetail.location}</li>
-            <li className="event-detail-item"><strong>City: </strong>{eventDetail.city.name}</li>
+            <li className="event-detail-item"><img className="date-icon" src="http://res.cloudinary.com/dirtnmtpc/image/upload/v1484560455/Time-And-Date-Calendar-icon_sxggmh.png" /><strong>Date: </strong>{eventDetail.date}</li>
+            <li className="event-detail-item"><img className="time-icon" src="http://res.cloudinary.com/dirtnmtpc/image/upload/v1484560455/42_hvqsub.png" /><strong>Start Time: </strong>{eventDetail.start_time}</li>
+            <li className="event-detail-item"><img className="time-icon" src="http://res.cloudinary.com/dirtnmtpc/image/upload/v1484560455/42_hvqsub.png" /><strong>End Time: </strong>{eventDetail.end_time}</li>
+            <li className="event-detail-item"><img className="location-icon" src="http://res.cloudinary.com/dirtnmtpc/image/upload/v1484560455/google-location-icon-16_fzuyro.png" /><strong>Location: </strong>{eventDetail.location}</li>
+            <li className="event-detail-item-city"><strong>City: </strong>{eventDetail.city.name}</li>
             <li className="event-detail-item"><strong>Sport Category: </strong>{eventDetail.category}</li>
             <li className="event-detail-item"><strong>Sport Level: </strong>{eventDetail.level}</li>
             <li className="event-detail-item"><strong>Number of Attendees: </strong>{eventDetail.attendees_num}</li>
             <li className="event-detail-item"><strong>Hosted by: </strong>{eventDetail.host.name}</li>
-            <li className="event-detail-item"><strong>Description: </strong></li>
-            <li className="event-detail-item">{eventDetail.description}</li>
+            <li className="event-detail-item-description-header"><strong>Description: </strong></li>
+            <li className="event-detail-item-description">{eventDetail.description}</li>
           </ul>
         </div>
         <div className="aside-event-detail col-third">
-          <h3>{`${eventDetail.attendings.length + 1} going`}</h3>
+          <h3 className="num-attendees">{`${eventDetail.attendings.length + 1} going`}</h3>
           <ul>
-            <li key={eventDetail.host.id}>
+            <li className="joining-member-list" key={eventDetail.host.id}>
               <div>{<img src={eventDetail.host.profile_img} className="member-attending-img"/>}</div>
-              <Link to={`/users/show/${eventDetail.host.id}`}><h3 className="member-name">{eventDetail.host.name}</h3></Link>
+              <Link to={`/users/show/${eventDetail.host.id}`}><h3 className="joining-member-name">{eventDetail.host.name}</h3></Link>
             </li>
             {eventDetail.attendings.map(member =>(
-              <li key={member.id}>
+              <li className="joining-member-list" key={member.id}>
                 <div>{<img src={member.profile_img} className="member-attending-img"/>}</div>
-                <Link to={`/users/show/${member.id}`}><h3 className="member-name">{member.name}</h3></Link>
+                <Link to={`/users/show/${member.id}`}><h3 className="joining-member-name">{member.name}</h3></Link>
               </li>
             ))}
           </ul>
