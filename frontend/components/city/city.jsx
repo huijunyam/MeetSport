0582f1abf7446db80b89f10f033d130d80914277
Hui Jun <hjyam@umich.edu>
2017-01-18 16:30:49 -0800

    Complete styling city home page

--- frontend/components/city/city.jsx
+++ frontend/components/city/city.jsx
@@ -6,7 +6,17 @@ import FooterContainer from '../layout/footer_container';
 class City extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      eventHover: "noColor",
+      memberHover: "noColor",
+      calendarHover: "noColor",
+      createHover: "noColor"
+     };
     this.handleClick = this.handleClick.bind(this);
+    this.handleEventHover = this.handleEventHover.bind(this);
+    this.handleMemberHover = this.handleMemberHover.bind(this);
+    this.handleCalendarHover = this.handleCalendarHover.bind(this);
+    this.handleCreateHover = this.handleCreateHover.bind(this);
   }
 
   componentDidMount() {
@@ -22,10 +32,46 @@ class City extends React.Component {
     const membership = { member_id: this.props.currentUser.id,
                          city_id: this.props.cityId };
     if (!this.props.hasMember) {
-      this.props.joinCity(membership).then(() => this.props.fetchEvents(this.props.cityId));
+      this.props.joinCity(membership).then(() => this.redirect());
     } else {
-      this.props.unjoinCity(this.props.membershipId.id);
+      this.props.unjoinCity(this.props.membershipId.id).then(() => this.redirect());
     }
+  }
+
+  handleEventHover() {
+    this.setState({
+      eventHover: "hoverClass",
+      memberHover: "noColor",
+      calendarHover: "noColor",
+      createHover: "noColor"
+    });
+  }
+
+  handleCalendarHover() {
+    this.setState({
+      eventHover: "noColor",
+      memberHover: "noColor",
+      calendarHover: "hoverClass",
+      createHover: "noColor"
+    });
+  }
+
+  handleMemberHover() {
+    this.setState({
+      eventHover: "noColor",
+      memberHover: "hoverClass",
+      calendarHover: "noColor",
+      createHover: "noColor"
+    });
+  }
+
+  handleCreateHover() {
+    this.setState({
+      eventHover: "noColor",
+      memberHover: "noColor",
+      calendarHover: "noColor",
+      createHover: "hoverClass"
+    });
   }
 
   render() {
@@ -39,13 +85,13 @@ class City extends React.Component {
           <button onClick={this.handleClick} className="sub-city-header-button">{buttonType}</button>
         </div>
         <div className="city-home-list">
-          <section className="city-sublist col-third">
-            <Link to={`/city/${this.props.cityId}/event`}><div className="city-sub-home-list">All Events</div></Link>
-            <Link to={`/city/${this.props.cityId}/member`}><div className="city-sub-home-list">All Members</div></Link>
-            <Link to={`/city/${this.props.cityId}/calendar`}><div className="city-sub-home-list">Calendar</div></Link>
-            <Link to={`/city/${this.props.cityId}/create`}><div className="city-sub-home-list">Create Event</div></Link>
+          <section className="city-sublist col-first">
+            <Link to={`/city/${this.props.cityId}/event`} onClick={this.handleEventHover}><div className={this.state.eventHover}>All Events</div></Link>
+            <Link to={`/city/${this.props.cityId}/member`} onClick={this.handleMemberHover}><div className={this.state.memberHover}>All Members</div></Link>
+            <Link to={`/city/${this.props.cityId}/calendar`} onClick={this.handleCalendarHover}><div className={this.state.calendarHover}>Calendar</div></Link>
+            <Link to={`/city/${this.props.cityId}/create`} onClick={this.handleCreateHover}><div className={this.state.createHover}>Create Event</div></Link>
           </section>
-          <section className="city-sublist-content col-last">
+          <section className="city-sublist-content col-lastest">
               <div>{this.props.children}</div>
           </section>
         </div>
