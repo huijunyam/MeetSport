f345f33223957eb6b5709fef1182f817f0f07538
Hui Jun <hjyam@umich.edu>
2017-01-20 10:47:09 -0800

    Complete styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ class User < ActiveRecord::Base
   validates :username, :email, :session_token, uniqueness: true
   validates :password, length: { minimum: 6 }, allow_nil: :true
   validate :validates_image_path
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   after_initialize :ensure_session_token
 
   has_many :memberships,
--- frontend/components/city/city_list_item.jsx
+++ frontend/components/city/city_list_item.jsx
@@ -4,28 +4,16 @@ import { Link } from 'react-router';
 class CityListItem extends React.Component {
   constructor(props){
     super(props);
-    this.state = { hoverClass: "hidden" };
-    this.handleMouseLeave = this.handleMouseLeave.bind(this);
-    this.handleMouseEnter = this.handleMouseEnter.bind(this);
-  }
-
-  handleMouseEnter() {
-    this.setState({ hoverClass: "showCity" });
-  }
-
-  handleMouseLeave() {
-    this.setState({ hoverClass: "hidden" });
   }
 
   render() {
     const {city} = this.props;
     return (
-      <li className="city-list-item">
+      <li className="city-list-item" >
         <div className="city-list-div">
           <Link to={`/city/${city.id}/event`} >
-            <img className="city-img" src={city["background_image_url"]}
-              onMouseOver={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}/>
-            <p className={this.state.hoverClass}>{city["name"]}</p>
+            <img className="city-img" src={city["background_image_url"]} />
+            <p className="city-name-list">{city["name"]}</p>
           </Link>
         </div>
       </li>
